d5ec95e9cbb8251817d5f4baf721b6836efcf33c
chyzhov-dev <chyzhov.dev@gmail.com>
2024-12-25 17:58:08 +0200

    feat: add step with app preview

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
   const [prompt, setPrompt] = useState<string>('');
   const [step, setStep] = useState<number>(0);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [previewLoading, setPreviewLoading] = useState<boolean>(false);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const onUserPromptChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setUserPrompt(e.target.value);
@@ -22,7 +24,8 @@ export default function Home() {
   };
 
   const onBackClick = () => {
-    setStep(0);
+    setStep((prev) => prev - 1);
+    setPreviewUrl(null);
   };
 
   const onCloseModal = () => {
@@ -54,16 +57,27 @@ export default function Home() {
             clearInterval(interval);
           }
         }, 10);
-
-        setIsLoading(false);
       })
-      .catch(() => {
-        setIsLoading(false);
-      });
+      .finally(() => setIsLoading(false));
   };
 
   const onBuildChatClick = () => {
     setModalOpen(true);
+  };
+
+  const onGetAppPreviewClick = async () => {
+    setPreviewLoading(true);
+
+    fetch('/api/ai/images', {
+      method: 'POST',
+      body: JSON.stringify({ q: prompt }),
+    })
+      .then((res) => res.json())
+      .then(({ url }) => {
+        setPreviewUrl(url);
+        setStep(2);
+      })
+      .catch(() => setPreviewLoading(false));
   };
 
   return (
@@ -111,14 +125,70 @@ export default function Home() {
                 onChange={onPromptChange}
               />
               <div className="flex gap-2">
-                <Button variant="primary" onClick={onBuildChatClick}>
+                <Button
+                  variant="primary"
+                  onClick={onGetAppPreviewClick}
+                  isLoading={previewLoading}
+                  disabled={previewLoading}
+                >
                   Get app preview
                 </Button>
                 <Button
                   variant="secondary"
-                  disabled={!prompt.trim().length}
+                  disabled={previewLoading}
                   onClick={onBackClick}
                 >
+                  Back
+                </Button>
+              </div>
+            </Step>
+          )}
+          {step === 2 && (
+            <Step
+              title="Step 3"
+              subtitle="Look over the preview and build the app"
+            >
+              {previewUrl && (
+                <div
+                  className={`relative max-w-full md:max-w-[80%] w-full aspect-square ${
+                    previewLoading
+                      ? 'animate-pulse bg-gray-300 flex items-center justify-center'
+                      : ''
+                  }`}
+                >
+                  <Image
+                    alt="Application preview"
+                    fill
+                    src={previewUrl}
+                    style={{ height: '100%', width: '100%' }}
+                    onLoadingComplete={() => setPreviewLoading(false)}
+                  />
+                  {previewLoading && (
+                    <svg
+                      className="w-8 h-8 stroke-gray-400"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20.5499 15.15L19.8781 14.7863C17.4132 13.4517 16.1808 12.7844 14.9244 13.0211C13.6681 13.2578 12.763 14.3279 10.9528 16.4679L7.49988 20.55M3.89988 17.85L5.53708 16.2384C6.57495 15.2167 7.09388 14.7059 7.73433 14.5134C7.98012 14.4396 8.2352 14.4011 8.49185 14.3993C9.16057 14.3944 9.80701 14.7296 11.0999 15.4M11.9999 21C12.3154 21 12.6509 21 12.9999 21C16.7711 21 18.6567 21 19.8283 19.8284C20.9999 18.6569 20.9999 16.7728 20.9999 13.0046C20.9999 12.6828 20.9999 12.3482 20.9999 12C20.9999 11.6845 20.9999 11.3491 20.9999 11.0002C20.9999 7.22883 20.9999 5.34316 19.8283 4.17158C18.6568 3 16.7711 3 12.9998 3H10.9999C7.22865 3 5.34303 3 4.17145 4.17157C2.99988 5.34315 2.99988 7.22877 2.99988 11C2.99988 11.349 2.99988 11.6845 2.99988 12C2.99988 12.3155 2.99988 12.651 2.99988 13C2.99988 16.7712 2.99988 18.6569 4.17145 19.8284C5.34303 21 7.22921 21 11.0016 21C11.3654 21 11.7021 21 11.9999 21ZM7.01353 8.85C7.01353 9.84411 7.81942 10.65 8.81354 10.65C9.80765 10.65 10.6135 9.84411 10.6135 8.85C10.6135 7.85589 9.80765 7.05 8.81354 7.05C7.81942 7.05 7.01353 7.85589 7.01353 8.85Z"
+                        stroke="stroke-current"
+                        strokeWidth="1.6"
+                        strokeLinecap="round"
+                      />
+                    </svg>
+                  )}
+                </div>
+              )}
+
+              {!previewUrl && (
+                <span className="bg-red-400">Something went wrong</span>
+              )}
+              <div className="flex gap-2">
+                <Button variant="primary" onClick={onBuildChatClick}>
+                  Build application
+                </Button>
+                <Button variant="secondary" onClick={onBackClick}>
                   Back
                 </Button>
               </div>
