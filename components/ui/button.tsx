5caceab1bac2c67cc7ad527238b074a81820e4d9
chyzhov-dev <chyzhov.dev@gmail.com>
2024-12-25 14:54:32 +0200

    feat: add Button ui component

---
+++ components/ui/button.tsx
@@ -0,0 +1,48 @@
+import { MouseEventHandler, ReactNode } from 'react';
+
+type Props = {
+  variant: 'primary' | 'secondary';
+  children: ReactNode;
+  className?: HTMLElement['className'];
+  onClick: MouseEventHandler;
+  disabled?: boolean;
+  isLoading?: boolean;
+};
+
+export default function Button({
+  children,
+  variant,
+  className,
+  onClick,
+  disabled,
+  isLoading,
+}: Props) {
+  const variantStyles = {
+    primary: 'bg-black text-white disabled:bg-gray-400 border-r-2',
+    secondary: 'border-black text-black border-2',
+  }[variant];
+
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`relative w-fit px-10 py-4 text-base ${
+        variantStyles ?? ''
+      } ${className}`}
+      type="button"
+    >
+      {isLoading && (
+        <div className="w-4 h-4 flex items-center justify-center absolute left-4">
+          <div className="loader w-4 h-4 border-t-4 border-b-4 border-t-blue-500 border-b-blue-500 rounded-full animate-spin" />
+        </div>
+      )}
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  isLoading: false,
+  disabled: false,
+  className: '',
+};
